f5bab314dfd10569cb932bd5446b2ff807e31840
Marlon Kerth <MarlonAACN@users.noreply.github.com>
2022-09-06 21:09:39 +0200

    fix tsc build to include config file

--- src/rules/order.ts
+++ src/rules/order.ts
@@ -5,7 +5,7 @@ const rule: Rule.RuleModule = {
   create: context => {
     return {
       JSXAttribute: (node: any) => {
-        if (!node.value || node.name.name !== "className" || !(typeof(node.value) === "string")) {
+        if (!node.value || node.name.name !== "className" || !(typeof(node.value.value) === "string")) {
           return;
         }
 
